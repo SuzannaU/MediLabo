7cb9b423c7930bef6d6e7ee48ecda6199a08ea29
SuzannaU <suzanna.ursulet@gmail.com>
2025-07-20 09:26:59 +0200

    spring security implemented in notes-service. authentication is working properly.

---
+++ medi-labo/notes-service/src/main/java/medilabo/notesapp/config/CustomProperties.java
@@ -0,0 +1,27 @@
+package medilabo.notesapp.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix="medilabo.user")
+public class CustomProperties {
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
