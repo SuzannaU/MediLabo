ffc63fce8cad1807a4106c1ad4e39254c49399ea
SuzannaU <suzanna.ursulet@gmail.com>
2025-08-05 10:11:27 +0200

    pom files are cleaned from unnecessary lines. parent pom specifies compiler version.

--- medi-labo/front-service/src/test/java/medilabo/frontapp/FrontServiceApplicationTests.java
+++ medi-labo/front-service/src/test/java/medilabo/frontapp/FrontServiceApplicationTests.java
@@ -2,8 +2,10 @@ package medilabo.frontapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class FrontServiceApplicationTests {
 
 	@Test
